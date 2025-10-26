e5be5cee42d99902196972be4d75f11111d7d7cc
shashi <shashipolity@gmail.com>
2025-10-26 14:23:06 +0530

    new folder setup added

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def main():
-    print("Hello from automated-research-report-generation!")
+from research_and_analyst.utils.model_loader import ModelLoader
 
-
-if __name__ == "__main__":
-    main()
+model_loader=ModelLoader()
+llm=model_loader.load_llm()
+print(llm.invoke("hi").content)
